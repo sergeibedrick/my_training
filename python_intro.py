36901d4e0121cbd7aa33bf046779af7745124176
sergeibedrick <sergeibedrick@gmail.com>
2024-09-20 16:07:34 +0300

    Add files via upload

---
+++ python_intro.py
@@ -0,0 +1,17 @@
+print("Hello word")
+
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print((9.99 > 9.98) and (1000 != 1000.1))
+
+# 3rd program
+a = 2 * 2 + 2
+print(a)
+b = 2 * (2 + 2)
+print(b)
+print(a == b)
+
+# 4th program
+print(int(float('123.456') * 10) % 10)
